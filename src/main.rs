b7f03c056af46b7837f789f9296420f7df4f41e9
wahlp <wahlp322@gmail.com>
2025-06-23 20:55:59 +0800

    use dark mode

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod app;
 use app::MyApp;
+use eframe::egui;
 
 fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "Video Compressor",
         native_options,
-        Box::new(|_cc| Ok(Box::new(MyApp::load().unwrap())))
-
+        Box::new(|creation_context| {
+            creation_context.egui_ctx.set_theme(egui::Theme::Dark);
+            Ok(Box::new(MyApp::load().unwrap()))
+        })
     )
 }
